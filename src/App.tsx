b360c47d52e73d308f180c7a2439545eaece4735
nefedovsv <serg.nefedoff@yandex.ru>
2019-08-25 22:37:41 +0300

    создал CRA, установил Antd

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import { Button } from "antd";
+const App: React.FC = () => {
+  return <Button type="primary">Button</Button>;
+};
+
+export default App;
