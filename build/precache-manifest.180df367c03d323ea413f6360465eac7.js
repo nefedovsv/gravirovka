678145813ba8c70f7eb28c88c4bd343a31563b30
nefedovsv <serg.nefedoff@yandex.ru>
2019-08-29 23:45:34 +0300

    создал сервер, сделал продакшн версию на порте 3002

---
+++ build/precache-manifest.180df367c03d323ea413f6360465eac7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d481cf1ba2f1ec65b466f4d5ce7f8ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082f0ba8969ce60f5619",
+    "url": "/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "b3a6a327ea1a09641593",
+    "url": "/static/css/main.6a622ff4.chunk.css"
+  },
+  {
+    "revision": "082f0ba8969ce60f5619",
+    "url": "/static/js/2.e0518808.chunk.js"
+  },
+  {
+    "revision": "b3a6a327ea1a09641593",
+    "url": "/static/js/main.4730cad1.chunk.js"
+  },
+  {
+    "revision": "415512b04ebca0bbb1fc",
+    "url": "/static/js/runtime~main.feb3c986.js"
+  },
+  {
+    "revision": "a383dc53ac95d2979b015627c0e90e0b",
+    "url": "/static/media/booking.a383dc53.jpg"
+  },
+  {
+    "revision": "fb9948ec507495493cb7f862ec2c3e07",
+    "url": "/static/media/coreldraw.fb9948ec.jpg"
+  },
+  {
+    "revision": "98dc5b3a95f6794c79a961485475354e",
+    "url": "/static/media/leather.98dc5b3a.jpg"
+  },
+  {
+    "revision": "d54747b44cfc36e9c0be493ed667d85b",
+    "url": "/static/media/logo.d54747b4.bmp"
+  },
+  {
+    "revision": "a7631aa647fa0e77d56ff60838abb3a2",
+    "url": "/static/media/metal.a7631aa6.jpg"
+  },
+  {
+    "revision": "fc6f4e856996dd43dc67771d0b933b6b",
+    "url": "/static/media/minimarker.fc6f4e85.jpg"
+  },
+  {
+    "revision": "eed140943a7832dc7f4abd4f71674d78",
+    "url": "/static/media/plastic.eed14094.jpg"
+  },
+  {
+    "revision": "fc2e911b0e24dcfd8d02b5c85c443779",
+    "url": "/static/media/post.fc2e911b.jpg"
+  }
+]);
